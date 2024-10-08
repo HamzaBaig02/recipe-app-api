7dae849e55321f45b062839a36d8ede99008ca54
Hamza Baig <baighamza02@gmail.com>
2024-10-08 23:43:58 +0500

    Add User api and Recipe api

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,47 @@
-#from django.contrib import admin
+from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as translate
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            translate('Permissions'),{
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (translate('Important dates'), {'fields':('last_login',)})
+    )
+
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            )
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
+
+admin.site.register(models.Recipe)
---
+++ app/recipe/views.py
@@ -0,0 +1,30 @@
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Recipe
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        #overiding queryset method to retirve recipes for self.user
+
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return super().get_serializer_class()
+
+
+
+
+
